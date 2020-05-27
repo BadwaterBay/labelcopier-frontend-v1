1e7d9c1f2f7eac3ad2d5d4cbe62d88752b253e26
tangaw <aaronwntang@gmail.com>
2020-05-26 17:08:54 -0700

    Restyle 'commit' button to better differentiate between disabled and active states

--- js/app.js
+++ js/app.js
@@ -227,6 +227,8 @@ $(document).ready(function () {
     $('#form-labels').text('');
     $('#commit-to-target-repo').text('Commit changes');
     $('#commit-to-target-repo').attr('disabled', true);
+    $('#commit-to-target-repo').removeClass('btn-raised btn-success')
+    $('#commit-to-target-repo').addClass('btn-outline-success');
   }
 
   function createNewLabelEntry(label, mode) {
@@ -382,6 +384,8 @@ $(document).ready(function () {
     $('#form-milestones').text('');
     $('#commit-to-target-repo').text('Commit changes');
     $('#commit-to-target-repo').attr("disabled", true);
+    $('#commit-to-target-repo').removeClass('btn-raised btn-success')
+    $('#commit-to-target-repo').addClass('btn-outline-success');
   }
 
   function createNewMilestoneEntry(milestone, mode) {
@@ -684,9 +688,13 @@ $(document).ready(function () {
 
     if (isNeeded) {
       $('#commit-to-target-repo').removeAttr('disabled');
+      $('#commit-to-target-repo').removeClass('btn-outline-success')
+      $('#commit-to-target-repo').addClass('btn-raised btn-success');
     }
     else {
       $('#commit-to-target-repo').attr("disabled", true);
+      $('#commit-to-target-repo').removeClass('btn-raised btn-success')
+      $('#commit-to-target-repo').addClass('btn-outline-success');
     }
   }
 
