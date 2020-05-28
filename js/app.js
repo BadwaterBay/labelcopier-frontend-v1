a25c18170882241eb953bc301725d6b7e37dc220
tangaw <aaronwntang@gmail.com>
2020-05-27 18:21:11 -0700

    Enable revert buttons only once user has listed labels/milestones

--- js/app.js
+++ js/app.js
@@ -515,7 +515,7 @@ $(document).ready(function () {
       apiCallGetEntries(targetOwner, targetRepo, kind, 'list', () => {
         $(this).button('reset');
       });
-
+      $(`#revert-${kind}-to-original`).removeAttr('disabled');
       $('#' + kind + '-tab').tab('show');
     }
     else {
