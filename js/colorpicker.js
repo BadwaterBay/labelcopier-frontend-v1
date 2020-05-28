8167a20b5ff93b735950bdd5ffc9b525692a620b
tangaw <aaronwntang@gmail.com>
2020-05-28 16:59:04 -0700

    Fix ESLint errors in colorpicker.js

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -280,7 +280,7 @@
 				return hex;
 			},
 			HexToRGB = function (hex) {
-				var hex = parseInt(((hex.indexOf('#') > -1) ? hex.substring(1) : hex), 16);
+				hex = parseInt(((hex.indexOf('#') > -1) ? hex.substring(1) : hex), 16);
 				return { r: hex >> 16, g: (hex & 0x00FF00) >> 8, b: (hex & 0x0000FF) };
 			},
 			HexToHSB = function (hex) {
