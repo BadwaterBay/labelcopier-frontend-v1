a06125946bda541ff70c097abfa8078754e9e2ec
Skyler Dong <dongskyler@gmail.com>
2020-05-30 18:35:52 -0500

    Moved serializeEntries() to an upper func scope

--- js/app.js
+++ js/app.js
@@ -834,67 +834,6 @@ $(document).ready(function () {
     commit();
   });
 
-  const serializeEntries = (jObjectEntry, kind) => {
-    const formatDate = (dateInput) => {
-      const date = dateInput.val();
-      const time = dateInput.attr('data-orig-time');
-
-      if (!date) {
-        return null;
-      }
-
-      const dt = {};
-      [dt.year, dt.month, dt.dayOfMonth] = date.split('-').map((e) => +e);
-      [dt.hour, dt.minute, dt.second] = time ? time.split(':') : [0, 0, 0];
-
-      const dateObject = new Date(
-        dt.year,
-        dt.month - 1,
-        dt.dayOfMonth,
-        dt.hour,
-        dt.minute,
-        dt.second,
-      );
-      return dateObject.toISOString().replace('.000Z', 'Z');
-    };
-
-    if (kind === 'labels') {
-      return {
-        name: jObjectEntry.find('[name="name"]').val(),
-        color: jObjectEntry.find('[name="color"]').val(),
-        description: jObjectEntry.find('[name="description"]').val(),
-        originalName: jObjectEntry.find('[name="name"]').attr('data-orig-val'),
-      };
-    } else if (kind === 'milestones') {
-      if (jObjectEntry.attr('data-number') !== 'null') {
-        return {
-          title: jObjectEntry.find('[name="title"]').val(),
-          state: jObjectEntry.find('[name="state"]').val(),
-          description: jObjectEntry.find('[name="description"]').val(),
-          due_on: formatDate(jObjectEntry.find('[name="due-date"]')),
-          number: +jObjectEntry.attr('data-number'),
-        };
-      } else {
-        if (jObjectEntry.find('[name="due-date"]').val() !== '') {
-          return {
-            title: jObjectEntry.find('[name="title"]').val(),
-            state: jObjectEntry.find('[name="state"]').val(),
-            description: jObjectEntry.find('[name="description"]').val(),
-            due_on: formatDate(jObjectEntry.find('[name="due-date"]')),
-          };
-        } else {
-          return {
-            title: jObjectEntry.find('[name="title"]').val(),
-            state: jObjectEntry.find('[name="state"]').val(),
-            description: jObjectEntry.find('[name="description"]').val(),
-          };
-        }
-      }
-    } else {
-      console.log('Bug in function serializeEntries!');
-    }
-  };
-
   const checkIfAnyEntryModified = () => {
     // returns true if any change has been made and activates or
     // disactivates commit button accordingly
@@ -949,6 +888,69 @@ $(document).ready(function () {
   };
 
   const commit = () => {
+    const serializeEntries = (jObjectEntry, kind) => {
+      const formatDate = (dateInput) => {
+        const date = dateInput.val();
+        const time = dateInput.attr('data-orig-time');
+
+        if (!date) {
+          return null;
+        }
+
+        const dt = {};
+        [dt.year, dt.month, dt.dayOfMonth] = date.split('-').map((e) => +e);
+        [dt.hour, dt.minute, dt.second] = time ? time.split(':') : [0, 0, 0];
+
+        const dateObject = new Date(
+          dt.year,
+          dt.month - 1,
+          dt.dayOfMonth,
+          dt.hour,
+          dt.minute,
+          dt.second,
+        );
+        return dateObject.toISOString().replace('.000Z', 'Z');
+      };
+
+      if (kind === 'labels') {
+        return {
+          name: jObjectEntry.find('[name="name"]').val(),
+          color: jObjectEntry.find('[name="color"]').val(),
+          description: jObjectEntry.find('[name="description"]').val(),
+          originalName: jObjectEntry
+            .find('[name="name"]')
+            .attr('data-orig-val'),
+        };
+      } else if (kind === 'milestones') {
+        if (jObjectEntry.attr('data-number') !== 'null') {
+          return {
+            title: jObjectEntry.find('[name="title"]').val(),
+            state: jObjectEntry.find('[name="state"]').val(),
+            description: jObjectEntry.find('[name="description"]').val(),
+            due_on: formatDate(jObjectEntry.find('[name="due-date"]')),
+            number: +jObjectEntry.attr('data-number'),
+          };
+        } else {
+          if (jObjectEntry.find('[name="due-date"]').val() !== '') {
+            return {
+              title: jObjectEntry.find('[name="title"]').val(),
+              state: jObjectEntry.find('[name="state"]').val(),
+              description: jObjectEntry.find('[name="description"]').val(),
+              due_on: formatDate(jObjectEntry.find('[name="due-date"]')),
+            };
+          } else {
+            return {
+              title: jObjectEntry.find('[name="title"]').val(),
+              state: jObjectEntry.find('[name="state"]').val(),
+              description: jObjectEntry.find('[name="description"]').val(),
+            };
+          }
+        }
+      } else {
+        console.log('Bug in function serializeEntries!');
+      }
+    };
+
     // freeze the world
     $('#loadingModal').modal({
       keyboard: false,
