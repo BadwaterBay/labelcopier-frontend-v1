cdbc7a2fe5bfd33b0858cc9f42aeb1b23d7cd726
Skyler Dong <dongskyler@gmail.com>
2020-06-03 18:03:44 -0500

    Update new project name and update README text

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,13 @@ const navbar = () => {
     <div class="navbar navbar-expand-md navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="#">
-          GitHub&nbsp;Label&nbsp;Manager
+          GitHub&nbsp;Label&nbsp;Manager&nbsp;Plus
         </a>
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
             <a
               class="nav-link"
-              href="https://github.com/BadwaterBay/github-label-manager-2"
+              href="https://github.com/BadwaterBay/github-label-manager-plus"
               target="_blank"
               rel="noreferrer"
             >
