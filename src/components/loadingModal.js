1c7d582b3f909ee9a966e4e5a2f86d4b29656542
tangaw <aaronwntang@gmail.com>
2020-06-03 16:52:50 -0700

    Vertically center modal

--- src/components/loadingModal.js
+++ src/components/loadingModal.js
@@ -1,7 +1,7 @@
 const loadingModal = () => {
   return `
     <div id="loadingModal" class="modal fade" tabindex="-1" role="dialog">
-      <div class="modal-dialog" role="document">
+      <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">
