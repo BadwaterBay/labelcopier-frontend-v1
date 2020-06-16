d20ff2ab5a08dcdab8413144cc5ba2e58588b938
Skyler Dong <dongskyler@gmail.com>
2020-06-16 07:17:22 -0500

    Add few scripts in package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es6: true,
     jquery: true,
   },
@@ -9,19 +8,18 @@ module.exports = {
     'google',
     'plugin:prettier/recommended',
     'plugin:css-modules/recommended',
-    'plugin:json/recommended',
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 2020,
     sourceType: 'module',
   },
   rules: {
     'prettier/prettier': 'error',
     'new-cap': 1,
   },
-  plugins: ['prettier', 'html', 'jquery', 'css-modules', 'markdown'],
+  plugins: ['prettier', 'html', 'jquery', 'css-modules',],
 };
