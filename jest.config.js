3479cf9b98b059764512991bccd64f5009d83568
Skyler Dong <dongskyler@gmail.com>
2020-08-07 23:37:13 -0500

    :white_check_mark: Add unit test for base64 (#195)

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  verbose: true,
+  testMatch: [
+    '<rootDir>/src/__tests__/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  testPathIgnorePatterns: ['(testCases).*$'],
+  collectCoverage: false,
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,ts}'],
+  coveragePathIgnorePatterns: ['<rootDir>/src/__tests__/'],
+  coverageDirectory: '<rootDir>/coverage/',
+};
---
+++ src/__tests__/base64.test.js
@@ -0,0 +1,31 @@
+/**
+ * Test base64
+ */
+
+'use strict';
+
+import base64 from '../js/base64';
+
+describe('Test functions', () => {
+  test('Test base64 encoder', () => {
+    // Input:
+    const input = [
+      'abcdef',
+      'BadwaterBay:github-label-manager-plus',
+      'BadwaterBay:template-for-label-and-milestone-1',
+    ];
+    // Output:
+    const output = [];
+    input.forEach((e) => {
+      output.push(base64.encode(e));
+    });
+    // Answer key:
+    const answerkey = [
+      'YWJjZGVm',
+      'QmFkd2F0ZXJCYXk6Z2l0aHViLWxhYmVsLW1hbmFnZXItcGx1cw==',
+      'QmFkd2F0ZXJCYXk6dGVtcGxhdGUtZm9yLWxhYmVsLWFuZC1taWxlc3RvbmUtMQ==',
+    ];
+    // Expect:
+    expect(output).toEqual(answerkey);
+  });
+});
